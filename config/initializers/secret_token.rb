07cfa059b9f6fce661248549958dd6263aa51787
Bregadeesh Samapathy <bregadeesh@gmail.com>
2011-08-20 17:33:13 +0800

    Application skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billshare::Application.config.secret_token = '8bddec029a7f0df64d11496b9426076a475c96ef02a93fbc9382a94d886ea6944660409e8918ceefde4628767f5ca71bd7faa0e2dc1ab01ccb04357d87cd8857'
